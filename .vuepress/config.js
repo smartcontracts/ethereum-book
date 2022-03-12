1efd5f6ca83cce60bdc8b899bc3984878a05568a
Kelvin Fichter <kelvin@optimism.io>
2022-03-12 14:03:08 -0500

    feat: add sidebar for zh-CN

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -33,7 +33,7 @@ module.exports = {
     lastUpdated: false,
     nav: [],
     sidebar: {
-      '/book/': [
+      '/book': [
         {
           title: 'Introduction',
           collapsable: false,
@@ -134,6 +134,110 @@ module.exports = {
             '/book/07__beacon-chain/01__validator-life-cycle.md',
             '/book/07__beacon-chain/02__validator-duties.md',
             '/book/07__beacon-chain/03__structure.md',
+          ]
+        },
+      ],
+      '/zh-CN/book': [
+        {
+          title: 'Introduction',
+          collapsable: false,
+          sidebarDepth: 1,
+          children: [
+            '/zh-CN/book/00__introduction/00__foreword.md',
+            '/zh-CN/book/00__introduction/01__motivation.md',
+            '/zh-CN/book/00__introduction/02__content.md',
+          ]
+        },
+        {
+          title: 'The Basics of Consensus',
+          collapsable: false,
+          sidebarDepth: 1,
+          children: [
+            '/zh-CN/book/01__consensus/00__index.md',
+            '/zh-CN/book/01__consensus/01__origins.md',
+            '/zh-CN/book/01__consensus/02__crash-faults.md',
+            '/zh-CN/book/01__consensus/03__byzantine-faults.md',
+            '/zh-CN/book/01__consensus/04__applications.md',
+          ]
+        },
+        {
+          title: 'The Origins of Blockchains',
+          collapsable: false,
+          sidebarDepth: 1,
+          children: [
+            '/zh-CN/book/02__blockchains/00__index.md',
+            '/zh-CN/book/02__blockchains/01__digital-cash.md',
+            '/zh-CN/book/02__blockchains/02__open-access.md',
+            '/zh-CN/book/02__blockchains/03__chains-of-blocks.md',
+            '/zh-CN/book/02__blockchains/04__fork-choice-rules.md',
+            '/zh-CN/book/02__blockchains/05__applications.md',
+            '/zh-CN/book/02__blockchains/06__participation.md',
+          ]
+        },
+        {
+          title: 'Revisiting Eth1',
+          collapsable: false,
+          sidebarDepth: 1,
+          children: [
+            '/zh-CN/book/03__eth1/00__index.md',
+            '/zh-CN/book/03__eth1/01__basics.md',
+            '/zh-CN/book/03__eth1/02__state.md',
+            '/zh-CN/book/03__eth1/03__transactions.md',
+            '/zh-CN/book/03__eth1/04__evm.md',
+            '/zh-CN/book/03__eth1/05__blocks.md',
+            '/zh-CN/book/03__eth1/06__fork-choice-rule.md',
+            '/zh-CN/book/03__eth1/07__clients.md',
+          ]
+        },
+        {
+          title: 'Exploring Eth2',
+          collapsable: false,
+          sidebarDepth: 1,
+          children: [
+            '/zh-CN/book/04__eth2-overview/00__index.md',
+            '/zh-CN/book/04__eth2-overview/01__historical-context.md',
+            '/zh-CN/book/04__eth2-overview/02__project-philosophy.md',
+            '/zh-CN/book/04__eth2-overview/03__key-concepts.md',
+            '/zh-CN/book/04__eth2-overview/04__development-process.md',
+            '/zh-CN/book/04__eth2-overview/05__learning-goals.md',
+          ]
+        },
+        {
+          title: 'Proof of Stake in Ethereum',
+          collapsable: false,
+          sidebarDepth: 1,
+          children: [
+            '/zh-CN/book/05__proof-of-stake/00__index.md',
+            '/zh-CN/book/05__proof-of-stake/01__core-concepts.md',
+            '/zh-CN/book/05__proof-of-stake/02__basic-operation.md',
+            '/zh-CN/book/05__proof-of-stake/03__fork-choice.md',
+            '/zh-CN/book/05__proof-of-stake/04__long-range-attacks.md',
+            '/zh-CN/book/05__proof-of-stake/05__casper-ffg.md',
+            '/zh-CN/book/05__proof-of-stake/06__weak-subjectivity.md',
+          ]
+        },
+        {
+          title: 'Eth2 Building Blocks',
+          collapsable: false,
+          sidebarDepth: 1,
+          children: [
+            '/zh-CN/book/06__building-blocks/00__index.md',
+            '/zh-CN/book/06__building-blocks/01__bls-signatures.md',
+            '/zh-CN/book/06__building-blocks/02__randomness.md',
+            '/zh-CN/book/06__building-blocks/03__ssz.md',
+            '/zh-CN/book/06__building-blocks/04__committees.md',
+            '/zh-CN/book/06__building-blocks/05__deposit-contract.md',
+          ]
+        },
+        {
+          title: 'Building the Beacon Chain',
+          collapsable: false,
+          sidebarDepth: 1,
+          children: [
+            '/zh-CN/book/07__beacon-chain/00__index.md',
+            '/zh-CN/book/07__beacon-chain/01__validator-life-cycle.md',
+            '/zh-CN/book/07__beacon-chain/02__validator-duties.md',
+            '/zh-CN/book/07__beacon-chain/03__structure.md',
           ]
         },
       ],
